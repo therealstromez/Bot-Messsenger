02beb41c2337c28f5be6b96297d05676129b4889
stromez <zemorts@gmail.com>
2023-03-02 20:23:05 +0700

    update smt

--- src/bot.py
+++ src/bot.py
@@ -18,8 +18,8 @@ class moifb(Client):
             )
 
         # log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
-        print(author_id)
-        print(self.uid)
+        # print(author_id)
+        # print(self.uid)
         if author_id != self.uid:
             if message_object.text:
                 if message_object.text == '/Getid' or message_object.text == '/getid':
@@ -31,17 +31,17 @@ class moifb(Client):
                     # cai nay de react
                     self.reactToMessage(message_object.uid, MessageReaction.LOVE)
                     # cai nay de change nickname
-                    self.changeNickname(message_object.text, message_object.author, thread_id=thread_id, thread_type=thread_type)
-                    self.sendLocalImage(
-                        "img/test/minh.jpg",
-                        message=Message(text="This is Minh ielts"),
-                        thread_id=thread_id,
-                        thread_type=thread_type,
-                    )
+                    # self.changeNickname(message_object.text, message_object.author, thread_id=thread_id, thread_type=thread_type)
+                    # self.sendLocalImage(
+                    #     "img/test/minh.jpg",
+                    #     message=Message(text="This is Minh ielts"),
+                    #     thread_id=thread_id,
+                    #     thread_type=thread_type,
+                    # )
                     responsechat = message_object.text
                     # cai nay de gui tin nhan
                     self.send(Message(
-                        text=responsechat),
+                        text=chat.response(responsechat)),
                         thread_id=thread_id,
                         thread_type=thread_type
                     )
